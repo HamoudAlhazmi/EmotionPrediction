bfb854775143270522f477afe0af4a496440eb01
HamoudAlhazmi <hamoudalhazmi@outlook.com>
2019-10-14 15:44:00 +1100

    cleaning the code

--- sentiment140/tfidftransformervstfidfvectorizer.py
+++ sentiment140/tfidftransformervstfidfvectorizer.py
@@ -19,29 +19,24 @@ word_count_vector = cv.fit_transform(docs)
 # Now, let’s check the shape. We should have 5 rows (5 docs) and 16 columns (16 unique words, minus single character words):
 print(word_count_vector.shape)
 
+# If smooth_idf=True (the default), the constant “1” is added to the numerator and denominator of the idf as if an extra
+# document was seen containing every term in the collection exactly once,
+# which prevents zero divisions: idf(d, t) = log [ (1 + n) / (1 + df(d, t)) ] + 1.
+# IDF) vector; only defined if use_idf is True.
 tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True)
 tfidf_transformer.fit(word_count_vector)
-
 # print idf values
 df_idf = pd.DataFrame(tfidf_transformer.idf_, index=cv.get_feature_names(),columns=["tf_idf_weights"])
-
 # sort ascending
 print(df_idf.sort_values(by=['tf_idf_weights']))
 
 # count matrix
 count_vector=cv.transform(docs)
-
-#print(count_vector)
-
 # tf-idf scores
 tf_idf_vector=tfidf_transformer.transform(count_vector)
-#print(tf_idf_vector)
-
 feature_names = cv.get_feature_names()
-
 # get tfidf vector for first document
 first_document_vector = tf_idf_vector[0]
-
 # print the scores
 df = pd.DataFrame(first_document_vector.T.todense(), index=feature_names, columns=["tfidf"])
 print(df.sort_values(by=["tfidf"], ascending=False))
