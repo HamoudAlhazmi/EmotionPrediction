05bb4c4f7f4defe2830f5f9dac2b96ec5af73b71
noeffort13 <62225577+noeffort13@users.noreply.github.com>
2020-04-26 08:53:01 +1000

    Improvement on PredictRecommend.py

--- 20newsgroups/PredictRecommend.py
+++ 20newsgroups/PredictRecommend.py
@@ -11,21 +11,47 @@ import pickle
 from joblib import dump, load
 
 path = r'PUT_YOUR_PATH_HERE'
+
 filename_model2 = 'model2.joblib'
 filename_vectorizer_model2 = 'vectorizer_model2.pickle'
 
-class SentimentPredict:
+filename_model2_class_dataframe = 'Model2_Class.csv'
+filename_article_database = 'article_database.csv'
+
+def predict(sentiment, path, filename_clf, filename_vectorizer):
+    clf = load(path + '\\' + filename_clf)
+    vectorizer = load(path + '\\' + filename_vectorizer)
+
+    text = [sentiment]
+    vectors = vectorizer.transform(text)
+    predicted = clf.predict(vectors)[0]
+    return predicted
+
+def import_csv_to_Dataframe(path, filename):
+    path = path + '\\' + filename
+    df = pd.read_csv(path)
+    return df
+
+def get_recommendation(predicted, path, filename_categories, filename_article_database):
+    df_categories = import_csv_to_Dataframe(path, filename_categories)
+    df_database = import_csv_to_Dataframe(path, filename_article_database)
+
+    article_id = df_categories.iloc[predicted, 2]
+    recommendation_article = df_database.iloc[int(article_id)].to_numpy()
+
+    return recommendation_article
+
+class Sentiment:
 
     def __init__(self, sentiment):
         self.sentiment = sentiment
         self.class_model1 = None
-        self.class_model2 = None
+        self.class_model2 = predict(sentiment, path, filename_model2, filename_vectorizer_model2)
 
-    def predict_model2(self):
-        clf = load(path + '\\' + filename_model2)
-        vectorizer = load(path + '\\' + filename_vectorizer_model2)
-
-        text = [self.sentiment]
-        vectors = vectorizer.transform(text)
-        self.class_model2 = clf.predict(vectors)[0]
-        return self.class_model2
+        recommendation_article = get_recommendation(self.class_model2, path, filename_model2_class_dataframe, filename_article_database)
+        self.title = recommendation_article[0]
+        self.description = recommendation_article[1]
+        self.author = recommendation_article[2]
+        self.category = recommendation_article[3]
+        self.source = recommendation_article[4]
+        self.link = recommendation_article[5]
