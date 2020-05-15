ad351c09451eea3071ef2b95f808ec82766dc0cb
Hamoud Alhazmi <56243454+HamoudAlhazmi@users.noreply.github.com>
2020-05-15 17:59:39 +1000

    Final Product

--- final/Django/home/views.py
+++ final/Django/home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from home.PredictRecommend import *
-from .test1 import car
+
 
 
 from .models import Post, Recommendation
